04510fb4c0d2ce7354c268735d318a92a3184781
Alex Kulagin <al.qlagin@gmail.com>
2019-11-28 23:35:19 +0500

    refactor: add PostWithAuthor interface

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,30 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from './user.service';
 
-interface Post {
-    id: number
-    userId: number
-    title: string
-    body?: string
-}
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PostsService {
-    private BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+  private BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-    constructor(
-        private readonly http: HttpClient
-    ) {
-    }
+  constructor(
+    private readonly http: HttpClient
+  ) {
+  }
 
-    fetch(): Observable<Post[]> {
-        return this.http.get<Post[]>(this.BASE_URL)
-    }
+  fetch(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.BASE_URL)
+  }
 
-    getById(id: number): Observable<Post> {
-        return this.http.get<Post>(`${this.BASE_URL}/${id}`)
-    }
+  getById(id: number): Observable<Post> {
+    return this.http.get<Post>(`${this.BASE_URL}/${id}`)
+  }
+}
+
+export interface Post {
+  id: number
+  userId: number
+  title: string
+  body?: string
+}
+
+export interface PostWithAuthor extends Post {
+  author: User;
 }
